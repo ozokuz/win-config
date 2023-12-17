602123bea0b349fe791203e406ea11d0d6ec0f22
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-12-18 01:12:48 +0200

    working oosu script

--- oosu.ps1
+++ oosu.ps1
@@ -1,12 +1,15 @@
 $folder = "$env:USERPROFILE\Tools"
+if (-not (Test-Path "$folder"))
+{
+    mkdir "$folder"
+}
+
 $exists = Test-Path "$folder\OOSU10.exe" -PathType Leaf
 if ($exists)
 {
     exit 0
 }
 
-mkdir $folder
-cd $folder
 Invoke-WebRequest "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile "$folder\OOSU10.exe"
 Invoke-WebRequest "https://raw.githubusercontent.com/ozokuz/win-config/main/ooshutup10.cfg" -UseBasicParsing -OutFile "$folder\ooshutup10.cfg"
-$folder\OOSU10.exe $folder\ooshutup10.cfg /quiet
+& $folder\OOSU10.exe $folder\ooshutup10.cfg /quiet
