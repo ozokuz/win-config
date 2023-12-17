99b9ba709e02c228bbe516d052d381a45ee12819
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-12-18 00:46:59 +0200

    first test version

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# win-config
-My Windows Configs
+# My Windows Configs
+
+## Machine Setup
+
+- Uninstall Some Default Apps
+- Unpin Start Menu Items
+- Microsoft Store Updates
+- Windows Updates
+- Driver Updates
+- Setup Dev Drive
+- Run base winget configuration
+- Run device specific winget configuration if available
---
+++ oosu.ps1
@@ -0,0 +1,11 @@
+$exists = Test-Path $env:USERPROFILE\Tools\OOSU10.exe -PathType Leaf
+if ($exists)
+{
+    exit 0
+}
+
+mkdir $env:USERPROFILE\Tools
+cd $env:USERPROFILE\Tools
+Invoke-WebRequest https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe -OutFile .\OOSU10.exe
+Invoke-WebRequest https://raw.githubusercontent.com/ozokuz/win-config/main/ooshutup10.cfg -UseBasicEncoding -OutFile .\oosu.txt
+.\OOSU10.exe .\ooshutup10.cfg /quiet
