040ca8b86ac1e6fde48206cda3417885f8e6897d
Ozoku <19756856+ozokuz@users.noreply.github.com>
2023-12-18 01:00:04 +0200

    try 3

--- oosu.ps1
+++ oosu.ps1
@@ -1,11 +1,12 @@
-$exists = Test-Path $env:USERPROFILE\Tools\OOSU10.exe -PathType Leaf
+$folder = "$env:USERPROFILE\Tools"
+$exists = Test-Path "$folder\OOSU10.exe" -PathType Leaf
 if ($exists)
 {
     exit 0
 }
 
-mkdir $env:USERPROFILE\Tools
-cd $env:USERPROFILE\Tools
-Invoke-WebRequest https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe -OutFile .\OOSU10.exe
-Invoke-WebRequest https://raw.githubusercontent.com/ozokuz/win-config/main/ooshutup10.cfg -UseBasicParsing -OutFile .\oosu.txt
-.\OOSU10.exe .\ooshutup10.cfg /quiet
+mkdir $folder
+cd $folder
+Invoke-WebRequest "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile "$folder\OOSU10.exe"
+Invoke-WebRequest "https://raw.githubusercontent.com/ozokuz/win-config/main/ooshutup10.cfg" -UseBasicParsing -OutFile "$folder\ooshutup10.cfg"
+$folder\OOSU10.exe $folder\ooshutup10.cfg /quiet
