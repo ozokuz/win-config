f75a586ab8332a3b854b9a42504c2ba865a8857c
Ozoku <19756856+ozokuz@users.noreply.github.com>
2024-08-25 21:41:28 +0300

    wow 3

--- run.ps1
+++ run.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/ozokuz/win-config/simplified/configuration.ps1 -OutFile "$env:USERPROFILE\Downloads\bootstrap.ps1"
-Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Verb RunAs -File $env:USERPROFILE\Downloads\bootstrap.ps1" -Wait
+Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $env:USERPROFILE\Downloads\bootstrap.ps1" -Wait -Verb RunAs
